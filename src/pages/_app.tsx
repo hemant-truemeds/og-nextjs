d5ca43d5a394b6fcde149e325d06f6cda315fb40
hemant-truemeds <hemant.jadhav@truemeds.in>
2023-05-02 19:17:48 +0530

    font poppins removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import Script from "next/script";
+// import Script from "next/script";
 import "@styles/global.css";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -9,18 +9,18 @@ import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
 import "@styles/swiper.css";
 import "react-loading-skeleton/dist/skeleton.css";
-import Head from "next/head";
+// import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <Script
+      {/* <Script
         strategy="lazyOnload"
         async
         defer
         src="https://kit.fontawesome.com/1c4700fdde.js"
         crossOrigin="anonymous"
-      />
+      /> */}
       <Component {...pageProps} />;
     </>
   );
