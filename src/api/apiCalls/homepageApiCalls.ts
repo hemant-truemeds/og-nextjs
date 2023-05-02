05e3b93b1a8ba77d9fa3486522769009fe50e98f
hemant-truemeds <hemant.jadhav@truemeds.in>
2023-05-02 12:56:07 +0530

    required api added

--- src/api/apiCalls/homepageApiCalls.ts
+++ src/api/apiCalls/homepageApiCalls.ts
@@ -1,17 +1,22 @@
-import { HOME_APIS } from "@api/apiConstants/homepageApi";
+import { HOME_APIS, HOME_STATIC_PAYLOAD } from "@api/apiConstants/homepageApi";
 import { axiosBase, axiosWp } from "@api/axiosClient";
 
 const HomepageApiCalls = async () => {
   try {
+    const elastic_search = await axiosBase.post(
+      HOME_APIS.ELASTIC_SEARCH,
+      HOME_STATIC_PAYLOAD.ELASTIC_SEARCH_PAYLOAD
+    );
     const bannerData = await axiosBase.post(
       HOME_APIS.BANNERS,
       JSON.stringify(["BANNERS", "ALERT", "HOME_GENERIC"])
     );
     const wpArticles = await axiosWp.get(HOME_APIS.WP_ARTICLES);
-    // console.log({ wpArticles });
+    // console.log({ elastic_search });
     return {
       bannerData: bannerData?.data,
       wpArticles: wpArticles?.data,
+      elastic_search: elastic_search?.data,
     };
   } catch (error) {
     console.log({ error });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,23 +6,35 @@ import HomeModule from "@modules/home";
 export interface IHomePage {
   bannerData: any;
   wpArticles: any;
+  elastic_search: any;
 }
 
 const Home: React.FC<IHomePage> = (props) => {
-  const { bannerData = {}, wpArticles = {} } = props;
-  console.log(props);
+  const { bannerData = {}, wpArticles = {}, elastic_search = {} } = props;
+  // console.log(props);
 
-  return <HomeModule bannerData={bannerData} wpArticles={wpArticles} />;
+  return (
+    <HomeModule
+      bannerData={bannerData}
+      wpArticles={wpArticles}
+      elastic_search={elastic_search}
+    />
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
-    const { bannerData = {}, wpArticles = {} } = await HomepageApiCalls();
+    const {
+      bannerData = {},
+      wpArticles = {},
+      elastic_search = {},
+    } = await HomepageApiCalls();
     // console.log("ssr", res?.bannerData);
     return {
       props: {
         bannerData,
         wpArticles,
+        elastic_search,
       },
     };
   } catch (error) {
@@ -32,6 +44,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       bannerData: {},
       wpArticles: {},
+      elastic_search: {},
     },
   };
 };
